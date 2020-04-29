e798e7e5ada766c2a941119c74c613216a7110d1
zhanghui <771662863@qq.com>
2020-04-29 11:52:45 +0800

    update

--- 2.18字符串令牌解析.py
+++ 2.18字符串令牌解析.py
@@ -1,11 +1,11 @@
 '''
-@Time    :   2020/04/29 11:39:31
+@Time    :   2020/04/29 11:51:26
 @Author  :   Zhang Hui
 '''
 
 # 问题：有一个字符串，想从左至右将其解析为一个令牌流
 
-# 字符串解析的高级用法，暂时跳过
+# 字符串解析的高级用法，代码不完整，暂时跳过
 
 import re
 
@@ -13,5 +13,12 @@ if __name__ == '__main__':
 
     # 把text转换为tokens序列对
     text = 'foo = 23 + 42 * 10'
-    tokens = [('NAME', 'foo'), ('EQ', '='), ('NUM', '23'), ('PLUS', '+'), ('NUM', '42'), ('TIMES', '*'), ('NUM', '10')
-    pass
+    tokens = [('NAME', 'foo'), ('EQ', '='), ('NUM', '23'),
+              ('PLUS', '+'), ('NUM', '42'), ('TIMES', '*'), ('NUM', '10')]
+    NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+    NUM = r'(?P<NUM>\d+)'
+    PLUS = r'(?P<PLUS>\+)'
+    TIMES = r'(?P<TIMES>\*)'
+    EQ = r'(?P<EQ>=)'
+    WS = r'(?P<WS>\s+)'
+    master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
