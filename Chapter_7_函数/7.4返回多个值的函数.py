5110b178800825a8839aaea0a2ac1a13c3eb74c6
zhanghui <771662863@qq.com>
2020-05-09 15:33:28 +0800

    Returning multiple values from a function

---
+++ Chapter_7_函数/7.4返回多个值的函数.py
@@ -0,0 +1,23 @@
+'''
+@Time    :   2020/05/09 15:27:05
+@Author  :   Zhang Hui
+'''
+
+if __name__ == '__main__':
+
+    def myfunc():
+        # 返回元组
+        return 1, 2, 3
+
+    a = myfunc()
+    print(a)
+
+    x, y, z = myfunc()
+    print(x, y, z)
+
+    # 元组解包
+    x, *y = myfunc()
+    print(x, y)
+
+    x, *y, z = myfunc()
+    print(x, y, z)
